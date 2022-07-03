de64a9c4549adf8c8b8840f5c843e450353d84bd
Eduardo Herrera <duds.herrera@gmail.com>
2022-07-03 14:24:01 -0300

    Adicionar arquivos de projeto.

---
+++ Logica 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont;
+
+            for (cont = 1; cont <= 50; cont++)
+            {
+                Console.WriteLine(cont);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
